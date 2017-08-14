41d8bceb7d2707235981e71c9373be39728ca87f
Szilard Szabo <szilard.szabo@falconsocial.com>
2017-08-14 14:37:33 +0200

    removing inspects

--- lib/scheduler.ex
+++ lib/scheduler.ex
@@ -8,7 +8,6 @@ defmodule GameOfLife.Scheduler do
   end
 
   def init(args) do
-    IO.inspect(args)
     schedule_work(args) # Schedule work to be performed at some point
     {:ok, args}
   end
@@ -21,17 +20,7 @@ defmodule GameOfLife.Scheduler do
     {:noreply, args}
   end
 
-  # def handle_cast(%ScheduledEvent{schedulerPid: scheduler}, state) do
-  #   IO.puts("handle_cast:")
-  #   IO.inspect(scheduler)
-  #   IO.inspect(state)
-  #   IO.puts("Scheduler stopped:" <> inspect(scheduler))
-  #   {:noreply, state}
-  # end
-
   defp schedule_work(args) do
-    IO.puts("schedule_work:")
-    IO.inspect(args)
     Process.send_after(self(), :work, args[:duration])
   end
 
