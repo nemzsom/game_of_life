e6d1def0c6835b80276b0913ff441ba094851ab2
Szilard Szabo <szilard.szabo@falconsocial.com>
2017-08-14 16:39:56 +0200

    Add read from file to the main

--- lib/game_of_life.ex
+++ lib/game_of_life.ex
@@ -2,11 +2,11 @@ defmodule GameOfLife do
   use Application
 
   def start(_type, args) do
-    world = [[:dead, :live, :dead],[:live, :dead, :live]]
+    world = GameOfLife.Pattern.load_world("block.world",{100,100})
     {:ok, wpid} = GameOfLife.World.start_link(world)
     {:ok, dpid} = GameOfLife.Display.start_link
     {:ok, simulation} = GameOfLife.Simulator.start_link([displayPid: dpid, worldPid: wpid])
-    {:ok, scheduler} = GameOfLife.Scheduler.start_link([duration: 4000, receiver: simulation]) #Supervisor.start_link([{GameOfLife.Scheduler ,[duration: 1000, receiver: simulation]}], strategy: :one_for_one)
+    {:ok, scheduler} = GameOfLife.Scheduler.start_link([duration: 5, receiver: simulation]) #Supervisor.start_link([{GameOfLife.Scheduler ,[duration: 1000, receiver: simulation]}], strategy: :one_for_one)
     {:ok, scheduler}
   end
 end
